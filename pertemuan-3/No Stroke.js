4009da01f5a288ae06b8283b660b741826556125
Hanifah Amaliya <hanifahamaliya16@gmail.com>
2023-05-14 19:11:08 +0700

    Laporan_21104410041_Hanifah Amaliya

---
+++ pertemuan-3/No Stroke.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(220);
+  fill(255,255,0,50);
+  //Garis Hijau
+  stroke(0,255,0);
+  circle(100,100,50);
+  //Garis Merah
+  stroke(255,0,0);
+  rect(0,0,100,50);
+  noFill();
+  //Garis Biru
+  noStroke();
+  fill(255,255,0);
+  ellipse(50,70,30,30);
+}
